f75f1ceabb0f4b96b69889712c091fc519292ea0
Paulo André Pimenta Aragão <andrearagao1003@outlook.com>
2022-12-27 23:06:31 -0300

    Game Platform Controller Update - Now it is only needed to change platform at MainMenu scene

--- Assets/Scripts/PCTests/GamePlatformController.cs
+++ Assets/Scripts/PCTests/GamePlatformController.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class GamePlatformController : MonoBehaviour
 {
-    public bool IsOnPC = true;
+    // Is platform a PC?
+    private bool IsOnPC;
 
-    // Start is called before the first frame update
-    void Awake()
+
+    /// <summary>
+    /// Set player according to the platform.
+    /// </summary>
+    /// <param name="isOnPc">Is platform a PC?</param>
+    public void SetGamePlatform(bool isOnPc)
     {
+        // Set IsOnPC variable
+        IsOnPC = isOnPc;
+
+        // If platform is PC, disable gyro controller and enable mouse controller
         if(IsOnPC)
         {
             GetComponentInChildren<GyroController>().enabled = false;
             GetComponentInChildren<MouseController>().enabled = true;
         }
+
+        // If platform is not PC, enable gyro controller and disable mouse controller
         else
         {
             GetComponentInChildren<GyroController>().enabled = true;
