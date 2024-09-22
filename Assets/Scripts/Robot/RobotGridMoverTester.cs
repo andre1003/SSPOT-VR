cb24402461b7109fa7958346f10e952cc1919823
lucas castro <lucas_o__castro@hotmail.com>
2024-09-22 18:15:31 -0300

    Animation test level

---
+++ Assets/Scripts/Robot/RobotGridMoverTester.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using Photon.Pun;
+using UnityEngine;
+
+namespace SSpot.Robot
+{
+    public class RobotGridMoverTester : MonoBehaviourPun
+    {
+        [SerializeField] private RobotGridMover mover;
+
+        private bool _isActing;
+
+        private IEnumerator ActCoroutine(IEnumerator action)
+        {
+            _isActing = true;
+            yield return action;
+            _isActing = false;
+        }
+        
+        private void Update()
+        {
+            if (_isActing) return;
+            
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                StartCoroutine(ActCoroutine(mover.MoveForwardCoroutine()));
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                StartCoroutine(ActCoroutine(mover.TurnLeftCoroutine()));
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                StartCoroutine(ActCoroutine(mover.TurnRightCoroutine()));
+            }
+        }
+    }
+}
