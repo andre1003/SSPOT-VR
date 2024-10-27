01230ec75a99ce718ea32597935024627669ff33
LucasOCastro <lucascastro@usp.br>
2024-10-26 23:22:50 -0300

    LinkedVectorAttribute to sync vector values

---
+++ Assets/Scripts/Editor/LabelWidthBlock.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace SSPot.Editor
+{
+    /// <summary>
+    /// A utility struct to temporarily set the label width in the Unity editor GUI.
+    /// When disposed, it restores the original label width.
+    /// </summary>
+    public readonly struct LabelWidthBlock : IDisposable
+    {
+        private readonly float _ogWidth;
+        
+        public LabelWidthBlock(float width)
+        {
+            _ogWidth = EditorGUIUtility.labelWidth;
+            EditorGUIUtility.labelWidth = width;
+        }
+
+        public LabelWidthBlock(GUIContent labelContent) : this(EditorStyles.label.CalcSize(labelContent).x)
+        {
+        }
+        
+        public void Dispose()
+        {
+            EditorGUIUtility.labelWidth = _ogWidth;
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/LinkedVectorAttribute.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace SSpot.Utilities
+{
+    /// <summary>
+    /// Draws a property with a toggle which links the values of the vector to be equal to each other.
+    /// </summary>
+    public class LinkedVectorAttribute : PropertyAttribute
+    {
+    }
+}
